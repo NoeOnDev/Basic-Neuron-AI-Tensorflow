bad4e4f78c5fd88714d04e832593aa6ab44ab061
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-28 04:04:27 -0600

    Corrección de errores en DFA para validar cadenas en español

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ dfa = DFA(
     input_symbols={'r', 'o', 'm', 'n', 'R', 'O', 'M', 'N'},
     transitions={
         'q0': {'r': 'q1', 'R': 'q1', 'o': 'qe', 'm': 'qe', 'n': 'qe', 'O': 'qe', 'M': 'qe', 'N': 'qe'},
-        'q1': {'o': 'q2', 'O': 'q2', 'r': 'q1', 'R': 'q1', 'm': 'qe', 'n': 'qe', 'M': 'qe', 'N': 'qe'},
-        'q2': {'m': 'q3', 'M': 'q3', 'o': 'q2', 'O': 'q2', 'r': 'qe', 'n': 'qe', 'R': 'qe', 'N': 'qe'},
+        'q1': {'o': 'q2', 'O': 'q2', 'r': 'qe', 'R': 'qe', 'm': 'qe', 'n': 'qe', 'M': 'qe', 'N': 'qe'},
+        'q2': {'m': 'q3', 'M': 'q3', 'o': 'qe', 'O': 'qe', 'r': 'qe', 'n': 'qe', 'R': 'qe', 'N': 'qe'},
         'q3': {'n': 'q4', 'N': 'q4', 'm': 'qe', 'M': 'qe', 'r': 'qe', 'o': 'qe', 'R': 'qe', 'O': 'qe'},
-        'q4': {'n': 'qe', 'N': 'qe', 'r': 'qe', 'o': 'qe', 'm': 'qe', 'R': 'qe', 'O': 'qe', 'M': 'qe'},
+        'q4': {'r': 'qe', 'o': 'qe', 'm': 'qe', 'n': 'qe', 'R': 'qe', 'O': 'qe', 'M': 'qe', 'N': 'qe'},
         'qe': {'r': 'qe', 'o': 'qe', 'm': 'qe', 'n': 'qe', 'R': 'qe', 'O': 'qe', 'M': 'qe', 'N': 'qe'}
     },
     initial_state='q0',
@@ -21,3 +21,5 @@ def es_valida(cadena):
         return True
     except:
         return False
+    
+print(es_valida("romN"))
